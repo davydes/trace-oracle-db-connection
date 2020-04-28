b390b5bea0c63cb50d1fd350c5c54d3d63a03232
Evgenii Davydov <e.davydov@lipt-soft.ru>
2020-04-28 09:59:25 +0300

    fails (exit 124) when longer that 1s with success

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -1,39 +1,47 @@
 import oracle.jdbc.OracleDriver
+import java.lang.RuntimeException
 import java.sql.DriverManager
 import java.sql.DriverManager.getConnection
 import java.sql.DriverManager.registerDriver
 import java.util.logging.LogManager
+import kotlin.system.exitProcess
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    val threshold = 1000L
     val connectionString = "jdbc:oracle:thin:iBSPB_Priv/Afpjbydthnjh_2016@10.74.95.100:1521/ibsodrb.bankspb.ru"
+
     configureLogging()
     configureJDBCDriver()
 
-    logTime("test finished") {
-        val connection = logTime("opened connection") {
-            getConnection(connectionString)
-        }
+    try {
+        logTime("test finished", threshold) {
+            val connection = logTime("opened connection") {
+                getConnection(connectionString)
+            }
 
-        try {
-            logTime("run select") {
-                connection.createStatement().use { statement ->
-                    val rs = statement.executeQuery("SELECT 'OK' as RESULT FROM DUAL")
-                    while (rs.next()) {
-                        val result = rs.getString("RESULT")
-                        println("got: $result")
+            try {
+                logTime("run select") {
+                    connection.createStatement().use { statement ->
+                        val rs = statement.executeQuery("SELECT 'OK' as RESULT FROM DUAL")
+                        while (rs.next()) {
+                            val result = rs.getString("RESULT")
+                            println("got: $result")
+                        }
                     }
                 }
-            }
-        } finally {
-            logTime("connection closed") {
-                connection.close()
+            } finally {
+                logTime("connection closed") {
+                    connection.close()
+                }
             }
         }
+    } catch (e: TimeoutThresholdException) {
+        exitProcess(124)
     }
 }
 
-fun <T> logTime(name: String, block: () -> T): T {
+fun <T> logTime(name: String, threshold: Long? = null, block: () -> T): T {
     var result: T? = null
     var throwable: Throwable? = null
     val millis = measureTimeMillis {
@@ -46,8 +54,11 @@ fun <T> logTime(name: String, block: () -> T): T {
     println("$name in ${millis}ms [${if (throwable != null) "FAILED" else "PASSED"}]")
 
     if (throwable != null) throw throwable!!
+    if (threshold != null && threshold < millis) throw TimeoutThresholdException()
     return result!!
 }
+
+class TimeoutThresholdException : RuntimeException()
 
 fun configureJDBCDriver() {
     registerDriver(OracleDriver())
@@ -55,7 +66,7 @@ fun configureJDBCDriver() {
 }
 
 fun configureLogging() {
-    System.setProperty("oracle.jdbc.Trace", "true")
+    //System.setProperty("oracle.jdbc.Trace", "true")
     val logManager = LogManager.getLogManager()
     (object {}).javaClass.getResourceAsStream("/logging.properties").use {
         logManager.readConfiguration(it)
